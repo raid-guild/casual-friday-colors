f2380f3fa4077a23f3b8fc8517994e05eef99e7c
skuhlmann <samkuhlmann@gmail.com>
2025-04-19 12:31:50 -0600

    paginate history

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,11 @@
-//export  const contractAddress = "0x4F4A3314B4461dA36ef81B9FA539F2B8d8a449cf";
-export const CONTRACT_ADDRESS = "0xF19DB47F7cCA0c3Aa501Ce61C158cbaE1830a3E7";
+import { base, sepolia } from "viem/chains";
+
+export const TARGET_CHAIN =
+  process.env.NEXT_PUBLIC_TARGET_CHAIN === "base" ? base : sepolia;
+export const CONTRACT_ADDRESS =
+  TARGET_CHAIN.id === base.id
+    ? "0xF19DB47F7cCA0c3Aa501Ce61C158cbaE1830a3E7"
+    : "0x62b79442D62d302201016512FBCB390aC9C6fe8A";
 export const MINT_PRICE = "100000000000000";
 
 export const WARPCAST_SHARE_URL = `https://warpcast.com/~/compose?text=I HAVE THE RAID COLOR!&embeds[]=https://colors.raidguild.org?color=`;
